7e6d9f1b34171244aa4ae5f40c11b9e693429e7a
RuoFeng207 <99081157@mydavinci.nl>
2026-03-22 12:36:59 +0100

    Shop
    Shop functioneel gemaakt en prijs berkent

--- gamecatalog/js/game_picker.js
+++ gamecatalog/js/game_picker.js
@@ -1,18 +1,26 @@
 const gamelist = document.querySelector(".gamelist");
 const filterSelect = document.getElementById("filter");
-
+const button = document.querySelector(".submit_button");
 let storgeGames = [];
 
-function displayGames(games) {
+function displayGames(gamesWithIndex, checkedIndices = []) {
     gamelist.innerHTML = '';
-
-    games.forEach((game, i) => {
+    const form = document.createElement("form");
+    gamelist.appendChild(form);
+    let sum = 0;
+    gamesWithIndex.forEach(({game, index}) => {
         const info = document.createElement("div");
-        info.classList.add("info-block","item");
+        info.classList.add("info-block", "item");
 
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.id = `game-${i}`;
+        checkbox.classList.add("checkbox");
+        checkbox.dataset.index = index;
+
+        if (checkedIndices.includes(index)) {
+            checkbox.checked = true;
+            sum += parseFloat(game.price);
+        }
 
         const textBox = document.createElement("div");
         textBox.classList.add("games-info");
@@ -25,15 +33,22 @@ function displayGames(games) {
 
         info.appendChild(checkbox);
         info.appendChild(textBox);
-        gamelist.appendChild(info);
+        form.appendChild(info);
     });
+
+    const totalprice = document.createElement("div");
+    totalprice.textContent = `Totaalprijs: €${sum.toFixed(2)}`;
+    totalprice.style.fontWeight = "bold";
+    totalprice.style.marginTop = "10px";
+    gamelist.appendChild(totalprice);
 }
 
 fetch('../json/games.json')
     .then(response => response.json())
     .then(data => {
         storgeGames = data;
-        displayGames(storgeGames);
+        const gamesWithIndex = storgeGames.map((game, i) => ({game, index: i}));
+        displayGames(gamesWithIndex);
     });
 
 filterSelect.addEventListener("change", () => {
@@ -48,5 +63,26 @@ filterSelect.addEventListener("change", () => {
         filteredGames = storgeGames.filter(game => parseFloat(game.price) > 20);
     }
 
-    displayGames(filteredGames);
+    const gamesWithIndex = filteredGames.map(game => ({game, index: storgeGames.indexOf(game)}));
+    displayGames(gamesWithIndex);
+});
+
+button.addEventListener("click", () => {
+    const selectedIndices = [];
+    const checkboxes = document.querySelectorAll(".gamelist input[type='checkbox']");
+
+    let sum = 0;
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            const index = parseInt(checkbox.dataset.index);
+            selectedIndices.push(index);
+            sum += parseFloat(storgeGames[index].price);
+        }
+    });
+
+    const selectedGamesWithIndex = selectedIndices.map(i => ({game: storgeGames[i], index: i}));
+    
+    displayGames(selectedGamesWithIndex, selectedIndices);
+    console.log(displayGames);
+    console.log("Totaalprijs:", sum);
 });
