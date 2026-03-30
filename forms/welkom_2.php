775b05b646371042135598d4cb7f3d63474f21e3
RuoFeng207 <99081157@mydavinci.nl>
2026-03-30 11:49:56 +0200

    form post

---
+++ forms/welkom_2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Welkom</title>
+</head>
+<body>
+<?php
+echo $_POST["name"]."<br>";
+echo $_POST["email"];
+
+?>
+    
+</body>
+</html>
